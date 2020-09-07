84a66677fae4af5876b88ecffba0e880ef20d1c8
Wilson Franca <wilson.franca.92@gmail.com>
2020-09-07 16:52:18 -0700

    function to format currency in BRL

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,9 @@
 const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+  Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(value);
 
 export default formatValue;
