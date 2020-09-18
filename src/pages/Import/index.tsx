181918b17d94533482a778ccaa6d37a16b39e949
Wilson Franca <wilson.franca.92@gmail.com>
2020-09-17 20:03:57 -0700

    completed import view

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,17 +23,28 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
-
+    const data = new FormData();
+    if (!uploadedFiles) return;
+    const file = uploadedFiles[0];
+    data.append('file', file.file, file.name);
+    // data.append('file[]', files.map(..)); for multiples files
+    // backend is importing only 1 file at a time - multer (single)
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
+      history.push('/');
     } catch (err) {
       console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const filesToBeUploaded = files.map(file => ({
+      file,
+      name: file.name,
+      readableSize: filesize(file.size),
+    }));
+
+    setUploadedFiles(filesToBeUploaded);
   }
 
   return (
